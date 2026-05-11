7ca544fd59d4e97dd519583e4ba2b610b1be1d7f
PracticalSage <marcus-bohman@hotmail.com>
2026-05-11 20:30:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ function moveElement(element, x, y) {
   }
 
   if (element.tagName === "polygon") {
-    const size = 0.38;
+    const size = 0.25;
     const points = `
       ${x},${y - size}
       ${x - size},${y + size}
