c67e5efe99062cf460bf2febd6829166ecdc6100
PracticalSage <marcus-bohman@hotmail.com>
2026-05-11 20:01:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("MoDo FF P18 coachboard laddad");
+const svg = document.querySelector("svg");
+const player = document.querySelector(".player-blue");
+
+let selectedElement = null;
+
+function getSvgPoint(event) {
+  const point = svg.createSVGPoint();
+
+  point.x = event.clientX;
+  point.y = event.clientY;
+
+  return point.matrixTransform(svg.getScreenCTM().inverse());
+}
+
+player.addEventListener("pointerdown", (event) => {
+  selectedElement = player;
+  player.setPointerCapture(event.pointerId);
+});
+
+player.addEventListener("pointermove", (event) => {
+  if (!selectedElement) return;
+
+  const position = getSvgPoint(event);
+
+  player.setAttribute("cx", position.x);
+  player.setAttribute("cy", position.y);
+});
+
+player.addEventListener("pointerup", () => {
+  selectedElement = null;
+});
