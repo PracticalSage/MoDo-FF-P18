e51ac9758870f5b2bc6dbb801052bc45a8ca167c
PracticalSage <marcus-bohman@hotmail.com>
2026-05-12 09:14:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ function addBall() {
   outer.setAttribute("r", 0.35);
   outer.setAttribute("fill", "white");
   outer.setAttribute("stroke", "#111");
-  outer.setAttribute("stroke-width", 0.08);
+  outer.setAttribute("stroke-width", 0.12);
 
   ball.appendChild(outer);
 
