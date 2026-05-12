345a913f771dc7892e8095bc4140aaa8d6dc7b3b
PracticalSage <marcus-bohman@hotmail.com>
2026-05-12 08:46:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ svg.addEventListener("touchend", () => {
 svg.addEventListener("mouseup", () => {
   if (drawingLine) finishPassLine();
   selected = null;
-
+});
 addBlueButton.addEventListener("click", () => addPlayer("blue"));
 addRedButton.addEventListener("click", () => addPlayer("red"));
 addConeButton.addEventListener("click", addCone);
